1125ea5ea4ebbeb87d305427089289873efbf458
bmmoore43 <bmjohns43@gmail.com>
2024-05-07 10:03:26 -0500

    wip

--- src/convert_seurat2anndata.R
+++ src/convert_seurat2anndata.R
@@ -20,32 +20,41 @@ use_condaenv(condaenv = '/w5home/bmoore/miniconda3/envs/scRNAseq_best/', require
 library(SeuratDisk)
 # variables
 WD <- "/w5home/bmoore/scRNAseq/GAMM/human_data/reh_cellrep_2020/output_seurat_mapping_20240125_112552/"
-SEURAT_OBJ <- "gamms2_cca_pred.rds"
-METADATA_GAMM <- "/w5home/bmoore/Gamm_scRNAseq/data/gamm_metadata/gammS2_manual_annot_metadata_c0.5.txt"
-OUTPUT_name <- "GAMM_S2_ortho_clabeled-clusters_0.5.h5Seurat"
+SEURAT_OBJ <- "human_D205_subset_annot.rds"
+METADATA_GAMM <- "NA" #"/w5home/bmoore/Gamm_scRNAseq/data/gamm_metadata/gammS2_manual_annot_metadata_c0.5.txt"
+OUTPUT_name <- "Reh_human_ortho_labeled-clusters.h5Seurat"
 
 setwd(WD)
 
 
 # read in Seurat object
-gamms2<- readRDS(file = SEURAT_OBJ)
+seurat <- readRDS(file = SEURAT_OBJ)
 
-# add in metadata from previous analysis
-metadata.gamm <- read.csv(METADATA_GAMM, row.names = 1,
-                          header = TRUE, sep = "\t")
-# check metadata with query data
-print(colnames(gamms2@assays$RNA@data)[1:10])
-print(rownames(metadata.gamm)[1:10])
-print(colnames(gamms2@meta.data))
-# add metadata to query data
+if(METADATA_GAMM=="NA"){
+  print("No metadata file provided")
+} else {
+    print("Metadata file provided")
+    print(METADATA_GAMM)
+    print("Reading in metadata")
+    # add in metadata from previous analysis
+    metadata.gamm <- read.csv(METADATA_GAMM, row.names = 1,
+                            header = TRUE, sep = "\t")
+    # check metadata with query data
+    print(colnames(seurat@assays$RNA@data)[1:10])
+    print(rownames(metadata.gamm)[1:10])
+    print(colnames(seurat@meta.data))
+
+    # add metadata to query data
+    seurat <- AddMetaData(seurat, metadata.gamm)
+    print(colnames(seurat@meta.data))
+}
+
 # remove "_1" from metadata
 #rownames(metadata.gamm) <- sub("_1", "", rownames(metadata.gamm))
 #rownames(metadata.gamm)[1:10]
-gamms2 <- AddMetaData(gamms2, metadata.gamm)
-print(colnames(gamms2@meta.data))
 
 # first save seurat as h5 seurat file
-SaveH5Seurat(gamms2, filename = OUTPUT_name)
+SaveH5Seurat(seurat, filename = OUTPUT_name)
 # then convert to h5ad
 Convert(OUTPUT_name, dest = "h5ad")
 
