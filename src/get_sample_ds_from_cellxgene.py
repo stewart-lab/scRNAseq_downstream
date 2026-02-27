2b2c6a7d19f772b359b0408a9210e414338b58cc
Yury V Bukhman <ybukhman@morgridge.org>
2026-02-27 10:49:30 -0600

    Added main()

--- src/get_sample_ds_from_cellxgene.py
+++ src/get_sample_ds_from_cellxgene.py
@@ -83,97 +83,81 @@ def subsample_by_cell_type(adata, num_cells_per_cell_type):
 # %% [markdown]
 # # Main script
 # %% [markdown]
-# ## Parse the config file and set parameters
-# Configuration dictionary and input and output directories
-# %%
-# Load configuration from config.json file
-config_dict = load_config()
+def main():
+    # ## Parse the config file and set parameters
+    # Load configuration from config.json file
+    config_dict = load_config()
 
-# Set input data directory
-DATA_DIR = get_data_dir(config_dict)
+    # Initialie output directory
+    out_dir = initialize_output_directory(config_dict)
 
-# Initialie output directory
-out_dir = initialize_output_directory(config_dict)
+    # Set custom parameters for the method
+    # Method name
+    METHOD = config_dict["METHOD"]
 
-# %% [markdown]
-# Set custom parameters for the method
-# %%
-# Method name
-METHOD = config_dict["METHOD"]
+    # Reference dataset(s)
+    census_version = (
+        config_dict[METHOD]["reference_datasets"]["census_version"]
+    )
+    organism = (
+        config_dict[METHOD]["reference_datasets"]["organism"]
+    )
+    ref_dataset_ids = (
+        config_dict[METHOD]["reference_datasets"]["ref_dataset_ids"]
+    )
 
-# Reference dataset(s)
-census_version = (
-    config_dict[METHOD]["reference_datasets"]["census_version"]
-)
-organism = (
-    config_dict[METHOD]["reference_datasets"]["organism"]
-)
-ref_dataset_ids = (
-    config_dict[METHOD]["reference_datasets"]["ref_dataset_ids"]
-)
+    # Number of cells per cell type in a subset of the reference
+    ref_cells_per_cell_type = (
+        config_dict[METHOD]["ref_cells_per_cell_type"]
+    )
 
-# Number of cells per cell type in a subset of the reference
-ref_cells_per_cell_type = (
-    config_dict[METHOD]["ref_cells_per_cell_type"]
-)
+    # Name of the output file that would contain the example subset
+    output_file = config_dict[METHOD]["output_file"]
 
-# Name of the output file that would contain the example subset
-output_file = config_dict[METHOD]["output_file"]
+    # Random seed
+    random_seed = config_dict[METHOD]["random_seed"]
 
-# Random seed
-random_seed = config_dict[METHOD]["random_seed"]
+    # ## Load the reference dataset from CellxGene
 
-# %% [markdown]
-# ## Load the reference dataset from CellxGene
+    # Create a census object
+    print("loading census version ", census_version, " ...")
+    census = cellxgene_census.open_soma(census_version=census_version)
 
-# %% [markdown]
-# Create a census object
+    # Load the reference dataset
+    print("loading reference dataset from CellxGene...")
+    adata_census = cellxgene_census.get_anndata(
+        census=census,
+        measurement_name="RNA",
+        organism=organism,
+        obs_value_filter=f"dataset_id in {ref_dataset_ids}",
+    )
+    print(adata_census)
 
-# %%
-print("loading census version ", census_version, " ...")
-census = cellxgene_census.open_soma(census_version=census_version)
+    # ## Sub-sample the data
+    # Create a random subset having ```ref_cells_per_cell_type``` representatives of each cell type. 
+    print("subsampling the reference dataset...")
 
-# %% [markdown]
-# Load the reference dataset
+    # Set random seed for reproducibility
+    np.random.seed(random_seed)
 
-# %%
-print("loading reference dataset from CellxGene...")
-adata_census = cellxgene_census.get_anndata(
-    census=census,
-    measurement_name="RNA",
-    organism=organism,
-    obs_value_filter=f"dataset_id in {ref_dataset_ids}",
-)
-print(adata_census)
+    # Subsample the reference dataset
+    adata_subset = subsample_by_cell_type(adata_census, ref_cells_per_cell_type)
+    print(adata_subset)
 
-# %% [markdown]
-# ## Sub-sample the data
-# Create a random subset having ```ref_cells_per_cell_type``` representatives of each cell type. 
+    # ## Save the generated dataset and package versions
 
-# %%
-print("subsampling the reference dataset...")
+    # Save the generated dataset
+    print(f"Saving generated dataset to {output_file} ...")
+    adata_subset.write_h5ad(out_dir + output_file)
 
-# Set random seed for reproducibility
-np.random.seed(random_seed)
+    # Save package versions
+    save_package_versions(out_dir)
 
-# Subsample the reference dataset
-adata_subset = subsample_by_cell_type(adata_census, ref_cells_per_cell_type)
-print(adata_subset)
+    # Set permissions for the output directory to be readable and writable by all users
+    os.chmod(out_dir, 0o777)
 
-# %% [markdown]
-# ## Save the generated dataset and package versions
+    # All done
+    print("Done.")
 
-# %%
-# Save the generated dataset
-print(f"Saving generated dataset to {output_file} ...")
-adata_subset.write_h5ad(out_dir + output_file)
-
-# %% Wrap up
-# Save package versions
-save_package_versions(out_dir)
-
-# Set permissions for the output directory to be readable and writable by all users
-os.chmod(out_dir, 0o777)
-
-# All done
-print("Done.")
+if __name__ == "__main__":
+    main()
