116838c204f97ecad53a65508760ed5ea505937e
Yury V Bukhman <ybukhman@morgridge.org>
2026-03-05 14:40:29 -0600

    Make adding noise to test dataset optional.

--- src/prep_test_ds.py
+++ src/prep_test_ds.py
@@ -123,42 +123,45 @@ def main():
     # Deep copy the adata object to avoid modifying the original data
     adata2 = adata.copy()
 
-    # ## Add noise to the count data
-    print("adding noise to the count data...")
-    # Set random seed for reproducibility
-    np.random.seed(random_seed)
-    # Add noise to the count data
-    adata2 = add_noise_to_counts(adata2, noise_level=noise_level)
-    print(adata2)
-
     # ## Modify cell ids by adding a prefix
     print("modifying cell ids by adding a prefix...")
     adata2 = modify_cell_ids(adata2, prefix=cell_id_prefix)
     print(adata2)
 
-    # ## Visualize the modified dataset compared to the original using UMAP
-    print("visualizing the modified dataset compared to the original using UMAP...")
+    # ## Add noise to the count data
+    if noise_level == 0:
+        print("no noise will be added to the count data since noise_level is set to 0")
+    else:
+        print("adding noise to the count data...")
+        # Set random seed for reproducibility
+        np.random.seed(random_seed)
+        # Add noise to the count data
+        adata2 = add_noise_to_counts(adata2, noise_level=noise_level)
+        print(adata2)
 
-    # Combine the original and modified datasets for visualization
-    adata_combined = ad.concat([adata, adata2], axis=0)
-    adata_combined.obs["dataset"] = ["original"] * adata.shape[0] + ["modified"] * adata2.shape[0]
+        # ## Visualize the modified dataset compared to the original using UMAP
+        print("visualizing the modified dataset compared to the original using UMAP...")
 
-    # Normalize and log-transform the combined dataset for visualization
-    sc.pp.normalize_total(adata_combined, target_sum=1e4)
-    sc.pp.log1p(adata_combined)
+        # Combine the original and modified datasets for visualization
+        adata_combined = ad.concat([adata, adata2], axis=0)
+        adata_combined.obs["dataset"] = ["original"] * adata.shape[0] + ["modified"] * adata2.shape[0]
 
-    # Select highly variable genes for visualization
-    sc.pp.highly_variable_genes(adata_combined, n_top_genes=2000, batch_key="dataset")
+        # Normalize and log-transform the combined dataset for visualization
+        sc.pp.normalize_total(adata_combined, target_sum=1e4)
+        sc.pp.log1p(adata_combined)
 
-    # Perform PCA and UMAP for visualization
-    sc.pp.pca(adata_combined)
-    sc.pp.neighbors(adata_combined, use_rep="X_pca")
-    sc.tl.umap(adata_combined)
-    (sc.pl.umap(
-        adata_combined, color="dataset", 
-        title="UMAP of Original vs Modified Dataset", show=False).
-        figure.savefig(out_dir + "umap_original_vs_modified.png")
-    )
+        # Select highly variable genes for visualization
+        sc.pp.highly_variable_genes(adata_combined, n_top_genes=2000, batch_key="dataset")
+
+        # Perform PCA and UMAP for visualization
+        sc.pp.pca(adata_combined)
+        sc.pp.neighbors(adata_combined, use_rep="X_pca")
+        sc.tl.umap(adata_combined)
+        (sc.pl.umap(
+            adata_combined, color="dataset", 
+            title="UMAP of Original vs Modified Dataset", show=False).
+            figure.savefig(out_dir + "umap_original_vs_modified.png")
+        )
 
     # ## Save the generated dataset and package versions
 
